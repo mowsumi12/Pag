18af18eac1951a1f87f9d3ab74df1e7d7fcd6575
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-05-24 16:33:37 +0600

    Add files via upload

---
+++ modules/events/leaveNoti.js
@@ -0,0 +1,34 @@
+module.exports.config = {
+	name: "leave",
+	eventType: ["log:unsubscribe"],
+	version: "1.0.0",
+	credits: "Mirai Team",
+	description: "Thông báo bot hoặc người rời khỏi nhóm",
+	dependencies: {
+		"fs-extra": "",
+		"path": ""
+	}
+};
+
+module.exports.run = async function({ api, event, Users, Threads }) {
+	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+	const { join } =  global.nodemodule["path"];
+	const { threadID } = event;
+	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  এখানে জিঁলাপিঁ থাকতে তুই লিভ নিবি 😺😸😹🤣😹" : "WELLCOME REMOVE🤧";
+	const path = join(__dirname, "cache", "leaveGif");
+	const gifPath = join(path, `leavvvv.mp4`);
+	var msg, formPush
+
+	if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+	(typeof data.customLeave == "undefined") ? msg = "ইস {name} {type} 🤖." : msg = data.customLeave;
+	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+
+	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
+	else formPush = { body: msg }
+	
+	return api.sendMessage(formPush, threadID);
+                                  }
